4e321a6a1874ae4169828d3613d3eed432d76b2c
Alvan29 <avinalvan29@gmail.com>
2025-08-07 10:58:04 +0700

    Delete foto

--- hapus_siswa.php
+++ hapus_siswa.php
@@ -3,15 +3,35 @@
     
     if(isset($_GET['id'])){
         $id = $_GET['id'];
-        $sql = "DELETE FROM data_siswa WHERE id=$id";
+        $sql = "SELECT foto FROM data_siswa WHERE id = '$id'";
         $query = mysqli_query($conn, $sql);
+        $query_foto = mysqli_fetch_assoc($query);
+        $foto = $query_foto['foto'];
 
-        if ($query){
-            header("location: data_siswa.php");
-        }else{
-            die("Gagal menghapus");
+        mysqli_begin_transaction($conn);
+        try{
+            $query_absensi = "DELETE FROM data_siswa WHERE id = '$id'";
+            mysqli_query($conn, $query_absensi);
+            if (!empty($foto)) {
+                $path_foto = __DIR__ . '/images/' . $foto;
+                
+                if (file_exists($path_foto)) {
+                    if (!unlink($path_foto)) {
+                        throw new Exception("Gagal menghapus file foto");
+                    }
+                }
+            }
+            mysqli_commit($conn);
+            $_SESSION['success'] = "Data siswa dan foto berhasil dihapus";
+            echo "i";
+        }catch (Exception $e) {
+            // Jika ada error, rollback transaksi
+            mysqli_rollback($conn);
+            $_SESSION['error'] = $e->getMessage();
+            echo "g";
         }
-    }else{
-        die("Akses dilarang");
-    }
+
+    header("location: data_siswa.php");
+    exit();
+}
 ?>
