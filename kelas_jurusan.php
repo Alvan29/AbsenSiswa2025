76daa6483931da757686ab34de806c8be724b22c
marzaananda <marzaanan21@gmail.com>
2025-08-17 19:57:26 +0700

    change css name

--- kelas_jurusan.php
+++ kelas_jurusan.php
@@ -53,7 +53,7 @@ if (isset($_GET['hapus'])) {
   <meta charset="UTF-8">
   <title>Data Kelas dan Jurusan</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/style.css" rel="stylesheet">
+  <link href="css/output.css" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
     function showHapusModal(id) {
